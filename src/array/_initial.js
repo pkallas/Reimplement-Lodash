6a014ef8f4948f8c8e3ec5d217b76f4c15c4e7cf
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-05-09 17:41:13 -0700

    Added initial function and initial test

---
+++ src/array/_initial.js
@@ -0,0 +1,12 @@
+// Gets all but the last element of array.
+
+// Create a function named _initial.
+function _initial(array) {
+// Remove last element from array
+  array.pop();
+// Return the modified array
+  return array;
+}
+
+// Export it.
+export default _initial;
