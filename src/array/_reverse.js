605abf7ffa2de464ce0e3bf5169b1f477339d91b
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-05-10 11:45:29 -0700

    Added reverse function and reverse test

---
+++ src/array/_reverse.js
@@ -0,0 +1,18 @@
+/* Reverses array so that the first element becomes the last,
+the second element becomes the second to last, and so on. */
+// Create a function named _reverse
+function _reverse(array) {
+// Create a variable of an empty array
+  var reversedArray = [];
+// Loop through the array
+  for (var i = 0; i <= array.length + 1; i++) {
+/* Remove the first element every iteration of the loop,
+and move it into the start of the empty array */
+    reversedArray.unshift(array.shift());
+  }
+// Return the previously empty array
+  return reversedArray
+}
+
+// Export it.
+export default _reverse;
