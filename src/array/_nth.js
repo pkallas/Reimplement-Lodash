fa2c6e3ba96cb7acf2ab24a21ef04dbd3016a34b
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-05-11 13:25:41 -0700

    Changed _nth function to run without modifying the starting array

--- src/array/_nth.js
+++ src/array/_nth.js
@@ -4,23 +4,23 @@ If n is negative, the nth element from the end is returned. */
 function _nth(array, number) {
 // Check to see if number is defined
   if (number === undefined) {
-    return array.shift();
+    return array[0];
   }
 // If number is defined, check to see if it's positive
   if (number > 0) {
-// Loop through the function n times, removing the first element each time
+/* Loop through the function n times, and return the element at the position
+where the loop ended */
     for (var i = 0; i < number; i++) {
-      array.shift();
-    }
-    return array.shift();
+      }
+    return array[i]
   }
 // If number is defined, check to see if it's negative
   if (number < 0) {
-// Loop through the function n times, removing the last element each time
-    for (var t = -1; t > number; t--) {
-      array.pop();
+/* Loop through the function n times, and return the element at the position
+where the loop ended */
+    for (var t = 0; t > number; t--) {
     }
-    return array.pop();
+    return array[array.length + t];
   }
 }
 
