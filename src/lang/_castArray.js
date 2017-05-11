691e2ddf2ed41c8383e1a5d3ef973087d456b3e4
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-05-11 09:33:44 -0700

    Added _castArray function and test

---
+++ src/lang/_castArray.js
@@ -0,0 +1,16 @@
+// Casts value as an array if it's not one.
+// Create a funtion named _castArray
+function _castArray(value) {
+// Create a variable that casts value as an array
+  var castValue = [value];
+// Check to see if value is an array
+  if (Array.isArray(value) === true) {
+// If true, do not change value
+    return value
+  }
+// Else, change value to be cast as an array
+  else return castValue
+}
+
+// Export it.
+export default _castArray;
