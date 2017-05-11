007f1423b67a0f9c8f906006b75cd9c7c5d1af50
Jonathan Pool <pool@panlex.org>
2017-05-11 10:42:32 -0700

    padStart function and correction of padEnd comment.

--- src/string/_padStart.js
+++ src/string/_padStart.js
@@ -1,5 +1,5 @@
 /*
-  Define a function to end-pad a specified string.
+  Define a function to start-pad a specified string.
 
   Preconditions:
     0. The count of arguments is 1, 2, or 3.
@@ -7,10 +7,10 @@
     2. Argument 1, if any, is an integer.
     3. Argument 2, if any, is a nonblank string.
 
-Note: This implementation does not use the String.prototype.padEnd method,
+Note: This implementation does not use the String.prototype.padStart method,
 because it is not included in ES2015.
 */
-var _padEnd = function(string, length, chars) {
+var _padStart = function(string, length, chars) {
   // If the arguments are valid:
   if (
     arguments.length > 0
@@ -40,12 +40,12 @@ var _padEnd = function(string, length, chars) {
       // Identify the amount required.
       var padNeed = length - string.length;
       // Identify the padding string.
-      var endPad =
+      var startPad =
         chars
         .repeat(Math.ceil(padNeed / chars.length))
         .substr(0, padNeed);
       // Return the padded string.
-      return string + endPad;
+      return startPad + string;
     }
     // Otherwise, i.e. if no padding is required:
     else {
@@ -61,4 +61,4 @@ var _padEnd = function(string, length, chars) {
 };
 
 // Export it.
-export default _padEnd;
+export default _padStart;
